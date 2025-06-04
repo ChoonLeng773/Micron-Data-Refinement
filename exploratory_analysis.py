094497bfa39cbe0ec3ba614660c557a0c2a08c37
Choon Leng <73415759+ChoonLeng773@users.noreply.github.com>
2025-06-04 23:30:53 +0800

    added matplot lib

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Read the Parquet file
 df = pd.read_parquet("train/incoming_run_data_1.parquet", engine="pyarrow")
@@ -6,3 +7,16 @@ df = pd.read_parquet("train/incoming_run_data_1.parquet", engine="pyarrow")
 print("Data from Parquet file:")
 print(df.columns)
 print(df.describe())
+
+
+# Describe and transpose for better plotting
+desc = df.describe().transpose()
+
+# Plotting the describe statistics
+desc[["mean", "std", "min", "max"]].plot(kind="bar", figsize=(12, 6))
+
+plt.title("Descriptive Statistics")
+plt.ylabel("Value")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
